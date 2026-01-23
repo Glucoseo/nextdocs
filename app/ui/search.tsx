330d0d663ce535d9c3ec211fb9351ea1749c0e37
Glucoseo <maqsotabdulaziz@gmail.com>
2026-01-23 18:43:32 +0500

    dashboard updates

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     else {
       params.delete('query');
+      replace(`${pathname}?${params.toString()}`);
     }
   }
   return (
