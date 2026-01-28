b80fafd01d1dcf82a4b4c70f76ba3a20e2373c2d
Glucoseo <maqsotabdulaziz@gmail.com>
2026-01-28 09:19:37 +0500

    Invoice changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogoDark from '@/app/ui/acme-logo-dark';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className={styles.shape}>
-        <AcmeLogo /> 
+        <AcmeLogoDark /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -27,6 +27,12 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Dashboard</span>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,10 +4,27 @@ import { lusitana } from '@/app/ui/fonts'
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white space-x-2  `}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
     </div>
   );
 }
+
+
+/*
+rt { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts'
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-black space-x-8`}
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg] -mt-2" />
+      <p className="text-[44px] -mt-2">Acme</p>
+    </div>
+  );
+}
+*/
