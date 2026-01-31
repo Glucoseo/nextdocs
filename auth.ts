159434d8ab4183d1fc36f5edd379f22e59e4db2c
Glucoseo <maqsotabdulaziz@gmail.com>
2026-01-31 23:30:21 +0500

    auth upd

--- auth.ts
+++ auth.ts
@@ -31,9 +31,11 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordMatch = await bcrypt.compare(password, user.password)
+          if (passwordMatch) return user;
         }
- 
-        return null;
+        console.log('Invalid credentials')
+        return null;  
       },
     }),
   ],
